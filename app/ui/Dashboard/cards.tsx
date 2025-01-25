09945e46aba01c565a2c05ae51b66d68b6eb1694
pranav605 <pranav605@gmail.com>
2025-01-24 23:48:24 -0500

    Styling changes and jounral page initialized

--- app/ui/Dashboard/cards.tsx
+++ app/ui/Dashboard/cards.tsx
@@ -1,14 +1,23 @@
 import { Journal } from "@/app/lib/defenitions"
 import clsx from "clsx"
 import Link from "next/link"
-export default async function CardsContainer({ journals }: { journals: Journal[]}) {
+export default async function CardsContainer({ journals }: { journals: Journal[] }) {
 
     return (
         <div className="flex justify-center items-center">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols- lg:grid-cols-4 xl:grid-cols-5 gap-4 bg-gray-900 text-white ">
                 {journals.map((journal) => {
                     const formattedDate = new Date(journal.created_on).toISOString().split('T')[0];
-                    return <Card key={journal.id} title={journal.title} createdon={formattedDate} isLocked={journal.locked} template={journal.template} />
+                    if(journal.locked){
+                        return (
+                            <Card key={journal.id} title={journal.title} createdon={formattedDate} isLocked={journal.locked} template={journal.template} />
+                        )
+                    }else{
+                    return (
+                        <Link key={journal.id} href={`/dashboard/${journal.id}`}>
+                            <Card key={journal.id} title={journal.title} createdon={formattedDate} isLocked={journal.locked} template={journal.template} />
+                        </Link>)
+                        }
                 })}
                 <NewJounralCard title="Journal" createdon="2025-01-23" />
             </div>
@@ -17,9 +26,9 @@ export default async function CardsContainer({ journals }: { journals: Journal[]
 }
 export function Card({ title, createdon, isLocked, template }: { title: string, createdon: string, isLocked: boolean, template: number }) {
     return (
-        <div className="relative mx-auto group hover:cursor-pointer active:scale-95">
-            <div className={clsx("absolute inset-2 rounded-md blur-md  bg-gradient-to-br from-pink-500 via-cyan-500 to-violet-500 z-10 opacity-0 md:group-hover:opacity-100 transition-opacity duration-300")}></div>
-            <div className={clsx("relative rounded-xl m-4 w-52 h-64 sm:w-52 sm:h-64 md:w-52 md:h-64 lg:w-60 lg:h-80 z-20 bg-cover bg-center",
+        <div className="relative group hover:cursor-pointer active:scale-95">
+            <div className={clsx("absolute inset-0 rounded-md blur-md w-52 h-64 sm:w-52 sm:h-64 md:w-52 md:h-64 lg:w-60 lg:h-80 bg-gradient-to-br from-pink-500 via-cyan-500 to-violet-500 z-10 opacity-0 md:group-hover:opacity-100 transition-opacity duration-300")}></div>
+            <div className={clsx("relative rounded-xl w-52 h-64 sm:w-52 sm:h-64 md:w-52 md:h-64 lg:w-60 lg:h-80 z-20 bg-cover bg-center",
                 { "bg-[url('/cover_1.jpg')] text-white": template === 1 },
                 { "bg-[url('/cover_2.jpg')] text-white": template === 2 },
                 { "bg-[url('/cover_3.jpg')] text-cyan-600": template === 3 },
@@ -39,13 +48,13 @@ export function Card({ title, createdon, isLocked, template }: { title: string, 
                     <p>{createdon}</p>
                 </div>
                 {isLocked ? (
-                    <div className="absolute top-4 right-4 rounded-full ">
+                    <div className="absolute top-4 left-5 rounded-full ">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                         </svg>
                     </div>
                 ) : (
-                    <div className="absolute top-4 right-4">
+                    <div className="absolute top-4 left-5 rounded-full">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                         </svg>
@@ -59,7 +68,7 @@ export function Card({ title, createdon, isLocked, template }: { title: string, 
 
 export function NewJounralCard({ title, createdon }: { title: string, createdon: string }) {
     return (
-        <Link href={'/dashboard/createJournal'} className="bg-gray-800 hover:cursor-pointer rounded-xl p-4 m-4 w-52 h-64 sm:w-52 sm:h-64 md:w-60 md:h-80 border-4 border-dotted border-gray-600 flex items-center justify-center">
+        <Link href={'/dashboard/createJournal'} className="bg-gray-800 hover:cursor-pointer rounded-xl w-52 h-64 sm:w-52 sm:h-64 md:w-52 md:h-64 lg:w-60 lg:h-80 border-4 border-dotted border-gray-600 flex items-center justify-center">
             <div className="text-center">
                 <h1 className="text-4xl">+</h1>
                 <p>Create a Journal</p>
